6c659937dcf3edfdfde5800d3c860005eeb2570c
brightUiMan <cassplus56@gmail.com>
2026-09-23 01:17:02 +0900

    tet33333411

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,7 +1,7 @@
 import { useGLTF } from '@react-three/drei'
 
 const Model = () => {
-    const { scene: base } = useGLTF( `${import.meta.env.BASE_URL}models/test/bus.glb`)
+    const { scene: base } = useGLTF( `${import.meta.env.BASE_URL}models/base.glb`)
     return (
         <>
 <primitive object={base}  />
