2534f4825e8474ad2dc88afa79f91aca2f609437
brightUiMan <cassplus56@gmail.com>
2026-09-22 23:22:09 +0900

    three 테스트

--- src/components/ThreeCanvas.tsx
+++ src/components/ThreeCanvas.tsx
@@ -1,5 +1,16 @@
+import { Canvas, useThree } from "@react-three/fiber";
 const ThreeCanvas = () => {
-    return <div>sdjlksadf</div>
+    return <>
+        <Canvas frameloop={"always" } camera={{ position: [0, 0, 0], rotation:[0, 0, 0],  fov: 60, near: 0.1, far: 1000 }} 
+
+         shadows >
+            <mesh position={[0, 0, -5]}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color="red"/>
+          </mesh>
+
+         </Canvas>
+    </>
 }
 
 export default ThreeCanvas;
